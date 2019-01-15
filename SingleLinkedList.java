b4c0d9e018d67128e0b31d424f0dbb3e5860af0b
Bendik, LB1BI <10798357+bendikiversen@users.noreply.github.com>
2019-01-15 15:50:37 +0100

    Default Constructor and constructor with args

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -3,6 +3,12 @@ public class SingleLinkedList
 	private Node head;
 	private int elementCount;
 
+	SingleLinkedList()
+	{
+		head = null;
+		elementCount = 0;
+	}
+
 	SingleLinkedList(Node head, int elementCount)
 	{
 		this.head = head;
