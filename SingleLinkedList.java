c7760d5d8b5bbe26bc3a0721e5de4b9359608ce6
Bendik, LB1BI <10798357+bendikiversen@users.noreply.github.com>
2019-01-15 15:45:59 +0100

    Constructor

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -2,4 +2,10 @@ public class SingleLinkedList
 {
 	private Node head;
 	private int elementCount;
+
+	SingleLinkedList(Node head, int elementCount)
+	{
+		this.head = head;
+		this.elementCount = elementCount;
+	}
 }
